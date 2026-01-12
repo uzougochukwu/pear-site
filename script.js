c77694c50399dfd77cd6024f01f7abcaf9312e34
uzougochukwu <uugochukwu@aol.com>
2026-01-12 21:15:56 +0000

    made api call to get EIP712 message triggered by button click

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 import walletAddress from "/env.js";
 
-function getEIP712Message(){
-    fetch(`https://hl-v2.pearprotocol.io/auth/eip712-message?address=${walletAddress}&clientId=APITRADER`, {
-     method: 'GET',
-     headers: {
-         "Accept": "*/*"
-     },
-})
+const auth = document.getElementById("auth");
+
+auth.addEventListener("click", (e) => {
+
+if (e.target.id === "getEIP"){ // remove the \ from the fetch url if you wish to use it in reqbin/postman/insomnia
+    fetch(`https://hl-v2.\
+pearprotocol.io/auth/eip712\
+-message?\
+address=${walletAddress}&clientId=APITRADER`, {
+    method: 'GET',
+    headers: {"Accept": "*/*"},})
 }
 
-const EIPdata = getEIP712Message();
+}
+)
+
+
+
 
 
