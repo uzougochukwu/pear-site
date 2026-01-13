6827772f17bc22641412c5cfee11816c2178e19d
uzougochukwu <uugochukwu@aol.com>
2026-01-13 16:41:21 +0000

    made message that only appears after successful api call to get eip-712

--- script.js
+++ script.js
@@ -2,18 +2,33 @@
 
 const auth = document.getElementById("auth");
 
+document.getElementById("wallet-test-outcome").setAttribute("hidden","");
+
 auth.addEventListener("click", (e) => {
 
 const walletAddress = document.getElementById('addresstext').value    
 
 if (e.target.id === "getEIP"){ // remove the \ from the fetch url if you wish to use it in reqbin/postman/insomnia
-    fetch(`https://hl-v2.\
+fetch(`https://hl-v2.\
 pearprotocol.io/auth/eip712\
 -message?\
 address=${walletAddress}&clientId=APITRADER`, {
     method: 'GET',
     headers: {"Accept": "*/*"},})
+    .then(res => res.json())
+    .then(data =>{
+console.log(data.message.action);
+    if (data.message.action == "authenticate"){
+        document.getElementById("wallet-test-outcome").removeAttribute("hidden","");
+    }
+    })
+
+
+
 }
+
+
+
 
 }
 )
