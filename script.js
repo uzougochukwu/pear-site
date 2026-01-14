51bebeb8120b1fb44625d47d187c2a27188940b6
uzougochukwu <uugochukwu@aol.com>
2026-01-14 19:04:35 +0000

    created signature function, need to link it to fix verfication

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //import walletAddress from "/env.js";
 
 const auth = document.getElementById("auth");
-//const login = document.getElementById("login");
+const login = document.getElementById("login");
 
 document.getElementById("wallet-test-outcome").setAttribute("hidden","");
 
@@ -9,6 +9,8 @@ var walletAddress="";
 var timestamp = 0;
 var signHash = "";
 var message = "";
+var signText = "";
+var signature = "";
 
 // getEIP712 message api call
 auth.addEventListener("click", (e) => {
@@ -31,37 +33,42 @@ address=${walletAddress}&clientId=APITRADER`, {
         message = data.message;
         
         connectRabbyWallet();
+        onSubmitWalletSign();
     }
-    console.log(data, timestamp, walletAddress);
+    // console.log(data, timestamp, walletAddress);
+    console.log(timestamp);
+    
     })
 }
 }
 )
 
 // authenticate user
-// login.addEventListener("click", (e) => {
+login.addEventListener("click", (e) => {
 
-//     if (e.target.id == "auth-login"){
-// fetch('https://hl-v2.pearprotocol.io/auth/login', {
-//     method: 'POST',
-//     headers: {
-//         "Content-Type": "application/json"
-//     },
-//     body: JSON.stringify({
-//         "method": "eip712",
-//         "address": walletAddress,
-//         "clientId": "APITRADER",
-//         "details": {
-//             "signature": signText,
-//             "timestamp": timestamp
-//         }
-//     })
-// }).then(res => res.json())
-// .then(data => {
-//     console.log(data);
-// })
-// }
-// })
+ const codeWalletSignature = document.getElementById('code-wallet-signature');
+
+    if (e.target.id == "auth-login"){
+fetch('https://hl-v2.pearprotocol.io/auth/login', {
+    method: 'POST',
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+        "method": "eip712",
+        "address": walletAddress,
+        "clientId": "APITRADER",
+        "details": {
+            "signature": codeWalletSignature.innerHTML,
+            "timestamp": timestamp
+        }
+    })
+}).then(res => res.json())
+.then(data => {
+    console.log(data);
+})
+}
+})
 
 
 async function connectRabbyWallet() {
@@ -83,6 +90,51 @@ async function connectRabbyWallet() {
     console.error("Rabby wallet is not installed");
   }
 }
+
+const onSubmitWalletSign = async (event) => {
+    console.group('onSubmitWalletSign');
+    //event.preventDefault();
+
+    // Retrieve message from input form with name "message"
+    //const message = event.currentTarget.message.value;
+    //console.log({ message });
+
+    // Store the message in a global variable
+    //MESSAGE = message;
+   // console.log(typeof(message));
+   // console.log(walletAddress);
+    
+    
+
+    // Get the element we want to output the result 
+    // of prompting the wallet for signature
+    const codeWalletSignature = document.getElementById('code-wallet-signature');
+    
+
+    // Prompt wallet for signature
+    try {
+        // Perform a personal sign with the original message and the wallet address
+        const signature = await window.ethereum.request({
+            method: 'personal_sign',
+            params: [JSON.stringify(message), walletAddress]
+        });
+        //console.log({ signature });
+        //console.log(signature);
+        //signText = signature;
+
+        // Show the signature
+        codeWalletSignature.innerHTML = signature;
+        //SIGNATURE = signature;
+        //console.log({ SIGNATURE });
+        console.log(codeWalletSignature.innerHTML);
+        
+    } catch (error) {
+        console.log({ error });
+       // codeWalletSignature.innerHTML = error?.message ?? 'Unknown wallet signature error.'
+    }
+
+    console.groupEnd();
+};
 
 
 
